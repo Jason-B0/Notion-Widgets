bc1180548b9cf2cd85d4495f1d136df1dabf0d81
JBai34 <jc_bai@outlook.com>
2023-11-08 10:14:04 -0400

    Added light and dark theme support

--- current_time_widget/time.js
+++ current_time_widget/time.js
@@ -19,3 +19,28 @@ function textClock() {
 }
 
 textClock();
+
+// DYNAMIC THEME SETTINGS BASED ON OS PREFERENCE
+// https://github.com/anthonytedja/notion/blob/main/time/script.js
+
+function light() {
+	document.documentElement.setAttribute('time-data-theme', 'light');
+}
+
+function dark() {
+	document.documentElement.setAttribute('time-data-theme', 'dark');
+}
+
+if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+	dark();
+} else {
+	light();
+}
+
+window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', (event) => {
+	if (event.matches) {
+		dark();
+	} else {
+		light();
+	}
+});
