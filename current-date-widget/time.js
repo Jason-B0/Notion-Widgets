b933a82855de61847256c9a54aa101d2683c63f6
JBai34 <jc_bai@outlook.com>
2023-11-10 15:43:42 -0400

    Feat. current date

--- current-date-widget/time.js
+++ current-date-widget/time.js
@@ -4,18 +4,39 @@ function textClock() {
 	
 	/* END SETTINGS */
 	var newDate = new Date(),
-		day = newDate.getDay(),
-		hours = newDate.getHours(),
-		minutes = newDate.getMinutes().toString(),
-		seconds = newDate.getSeconds().toString();
-		milliseconds = newDate.getMilliseconds.toString();
-
-	if (useMilitaryTime == false && (hours > 12 && hours !== 0 && hours !== 23)) {
-		hours = hours - 12;
+		year = newDate.getFullYear(),
+		month = newDate.getMonth(),
+		date = newDate.getDate(),
+		day = newDate.getDay()
+	
+	switch (day) {
+		case 0:
+			day = "Sunday";
+			break;
+		case 1:
+			day = "Monday";
+			break;
+		case 2:
+			day = "Tuesday";
+			break;
+		case 3:
+			day = "Wednesday";
+			break;
+		case 4:
+			day = "Thursday";
+			break;
+		case 5:
+			day = "Friday";
+			break;
+		case 6:
+			day = "Saturday";
+			break;
+		default:
+			break;
 	}
 	
 	// edit inner HTML directly
-	document.getElementById("time").innerHTML = hours + ":" + minutes.padStart(2, '0') + ":" + seconds.padStart(2, '0') + ":" + milliseconds.padStart(3, '0');
+	document.getElementById("time").innerHTML = year.toString() + "-" + month.toString() + "-" + date.toString() + "\t\t" + day.toString();
 	setTimeout(textClock, 1000);
 }
 
