214c739e29f09b523d327d5850ecad6a6ecbf586
JBai34 <jc_bai@outlook.com>
2025-02-24 09:43:19 -0400

    Feat. time widget to display both local and UTC time

--- current-time-widget/time.js
+++ current-time-widget/time.js
@@ -1,21 +1,16 @@
-function textClock() {
-	/* SETTINGS */
-	var useMilitaryTime = true;
-	
-	/* END SETTINGS */
-	var newDate = new Date(),
-		day = newDate.getDay(),
-		hours = newDate.getHours(),
-		minutes = newDate.getMinutes().toString(),
-		seconds = newDate.getSeconds().toString();
-		milliseconds = newDate.getMilliseconds().toString();
+/* SETTINGS */
+const USE_MILITARY_TIME = true;
 
-	if (useMilitaryTime == false && (hours > 12 && hours !== 0 && hours !== 23)) {
-		hours = hours - 12;
-	}
+function getZuluTime ()
+{
+	let newDate = new Date(),
+		hours = newDate.getUTCHours(),
+		minutes = newDate.getUTCMinutes().toString(),
+		seconds = newDate.getUTCSeconds().toString();
+		milliseconds = newDate.getUTCMilliseconds().toString();
 	
 	// edit inner HTML directly	
-	document.getElementById("time").innerHTML =
+	document.getElementById("zulu-time-txt").innerHTML =
 		hours.toString().padStart(2, '0')
 		+ ":"
 		+ minutes.padStart(2, '0')
@@ -24,8 +19,47 @@ function textClock() {
 		+ ":"
 		+ milliseconds.padStart(3, '0');
 	
-	setTimeout(textClock, 50);
+	setTimeout(getZuluTime, 50);	
 }
 
-textClock();
+function getLocalTime ()
+{
+	let newDate = new Date(),
+		day = newDate.getDay(),
+		hours = newDate.getHours(),
+		minutes = newDate.getMinutes().toString(),
+		seconds = newDate.getSeconds().toString();
+		milliseconds = newDate.getMilliseconds().toString();
+
+	if ( USE_MILITARY_TIME == false && (hours > 12 && hours !== 0 && hours !== 23)) {
+		hours = hours - 12;
+	}
+	
+	// edit inner HTML directly	
+	document.getElementById("local-time-txt").innerHTML =
+		hours.toString().padStart(2, '0')
+		+ ":"
+		+ minutes.padStart(2, '0')
+		+ ":"
+		+ seconds.padStart(2, '0')
+		+ ":"
+		+ milliseconds.padStart(3, '0');
+	
+	setTimeout( getLocalTime, 50);
+}
+
+function displayTimezone ()
+{
+	let timezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+	let offset = new Date().getTimezoneOffset() / -60;
+	
+	timezone = timezone.split( "/" ).pop().replace( "_", " " );
+	timezone = `${ timezone } (UTC${ offset >= 0 ? '+' : '' }${ offset })`;
+	
+	document.getElementById("timezone-txt").innerHTML = timezone;
+}
+
+displayTimezone();
+getLocalTime();
+getZuluTime()
 
